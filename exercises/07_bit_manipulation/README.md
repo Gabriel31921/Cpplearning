9858f0026670fd99c0fced3e33c53afe619b6267
Gabriel Borrego <gborrol585@gmail.com>
2025-07-11 16:59:10 +0200

    8th exercise, worked with bit flags, bit masks and the bitwise operators, introductory exercise

--- exercises/07_bit_manipulation/README.md
+++ exercises/07_bit_manipulation/README.md
@@ -1,34 +1,38 @@
 **Problem:**
 
-Implement a function that does a left rotation on a `std::bitset<4>`.
-Do it here:
+Do not use `std::bitset` in this quiz. We’re only using `std::bitset` for printing.
+
+Given the following program:
 
 ```c++
-
 #include <bitset>
+#include <cstdint>
 #include <iostream>
-
-// "rotl" stands for "rotate left"
-std::bitset<4> rotl(std::bitset<4> bits)
-{
-// Your code here
-}
 
 int main()
 {
-	std::bitset<4> bits1{ 0b0001 };
-	std::cout << rotl(bits1) << '\n';
+    [[maybe_unused]] constexpr std::uint8_t option_viewed{ 0x01 };
+    [[maybe_unused]] constexpr std::uint8_t option_edited{ 0x02 };
+    [[maybe_unused]] constexpr std::uint8_t option_favorited{ 0x04 };
+    [[maybe_unused]] constexpr std::uint8_t option_shared{ 0x08 };
+    [[maybe_unused]] constexpr std::uint8_t option_deleted{ 0x10 };
 
-	std::bitset<4> bits2{ 0b1001 };
-	std::cout << rotl(bits2) << '\n';
+    std::uint8_t myArticleFlags{ option_favorited };
 
-	return 0;
+    // Place all lines of code for the following quiz here
+
+    std::cout << std::bitset<8>{ myArticleFlags } << '\n';
+
+    return 0;
 }
-
 ```
+
+1. Add a line of code to set the article as viewed.
+2. Add a line of code to check if the article was deleted.
+3. Add a line of code to clear the article as a favorite.
 
 **Solution:**
 
-Given a 4-bit value, for rotating it we have to know the value in the index 3,
-or the most significant bit. If said value is 0, then just do a left shift, and if it's one,
-then do a left shift and flip the value in index 0, turning it on.
+We start with the `option_favorite` flag set, corresponding to bit position 2 turned on, 
+then we have to set bit position 0, this is `option_viewed`. This would result in `0000'0101`.
+Then we test if bit position 4 is set, and at last, clear the bit position 2, to end with `0000'0001`.
--- exercises/07_bit_manipulation/main.cpp
+++ exercises/07_bit_manipulation/main.cpp
@@ -2,33 +2,43 @@
 // Created by gborr on 26/06/2025.
 //
 #include <bitset>
+#include <cstdint>
 #include <iostream>
 
 /*
  * Go to the README for this one...
  */
 
-// "rotl" stands for "rotate left"
-std::bitset<4> rotl(std::bitset<4> bits) {
-    const bool value {bits.test(3)};     //Test if the value in the MSB is true or false.
-    if (value) {                                //If true, do a left shift and turn on the LSB
-        bits <<= 1;
-        bits.set(0);
-    }
-    else {                                      //If false, just do a left shift
-        bits <<= 1;
-    }
-
-    return bits;
-}
-
 int main()
 {
-    std::bitset<4> bits1{ 0b0001 };
-    std::cout << rotl(bits1) << '\n';
+    [[maybe_unused]] constexpr std::uint8_t option_viewed{ 0x01 };
+    [[maybe_unused]] constexpr std::uint8_t option_edited{ 0x02 };
+    [[maybe_unused]] constexpr std::uint8_t option_favorited{ 0x04 };
+    [[maybe_unused]] constexpr std::uint8_t option_shared{ 0x08 };
+    [[maybe_unused]] constexpr std::uint8_t option_deleted{ 0x10 };
 
-    std::bitset<4> bits2{ 0b1001 };
-    std::cout << rotl(bits2) << '\n';
+    std::uint8_t myArticleFlags{ option_favorited };
+
+    // Place all lines of code for the following quiz here
+    myArticleFlags |= option_viewed;
+    /*
+     * This bitwise operator OR "|" sees that there is a 1 in bit position 0 in option_viewed, this is, it's set.
+     * Then, it sets the same position in myArticleFlags if it was not set before, if it was set, it was, it does nothing.
+     */
+    std::cout << "Was the article deleted?" << ((myArticleFlags & option_deleted)? " True" : " False") << std::endl;
+    /*
+     * The AND operator checks if the bit position 4 is set in both myArticleFlags and option_deleted at the same time,
+     * if it is, returns true, if not, false.
+     */
+    myArticleFlags &= ~option_favorited;
+    /*
+    * This line clears the "favorited" flag (bit position 2) in myArticleFlags.
+    * ~option_favorited inverts all bits, turning bit 2 to 0 and all others to 1.
+    * The bitwise AND "&" then clears only bit 2, preserving the rest.
+    * This operation ensures the article is no longer marked as favorited.
+    */
+
+    std::cout << std::bitset<8>{ myArticleFlags } << '\n';
 
     return 0;
 }
